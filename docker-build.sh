1ed640b9e60feb7bc2b48b7bd7dc22851210f16e
carvilsi <carvilsi.mail.list@gmail.com>
2018-01-09 14:01:26 +0100

    midifying the docker scripts to read name and version from packcage.json file

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# docker build -t sysunite/weaver-server:3.3.2-beta.1 .
-echo sysunite/`node -p "require('./package.json').name"`:`node -p "require('./package.json').version"`
+docker build -t sysunite/`node -p "require('./package.json').name"`:`node -p "require('./package.json').version"` .
